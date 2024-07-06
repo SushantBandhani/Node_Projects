d7cd0e38549e42d7f2771fd90caa5ad0dd09310b
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-06 12:36:52 +0530

    Adding users.js

---
+++ Voting app/models/users.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    aadharCardNumber: {
+        type: Number,
+        required: true,
+        unqiue: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['voter', 'admin'],
+        default: 'voter'
+    },
+    isVoted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+userSchema.pre('save', async function(next){
+    const person = this;
+
+    if(!person.isModified('password')) return next();
+    try{
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
