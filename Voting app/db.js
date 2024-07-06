a216eb7f07d140f286f70a4fdfbebcfa2f276b9e
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-06 12:33:33 +0530

    Initializing db.js file

---
+++ Voting app/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURL = process.env.MONGODB_URL_LOCAL 
+// const mongoURL = process.env.MONGODB_URL;
+// Set up MongoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+module.exports = db;
