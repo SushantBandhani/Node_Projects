a4aebe21d373e763d99f2dee862bb3132163f967
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-17 10:57:19 +0530

    Adding controllers functions

---
+++ URL_Shortner app/controllers/url.js
@@ -0,0 +1,33 @@
+const shortid=require("shortid")
+
+//Importing database
+const URL=require("../models/url")
+
+async function handleGenerateNewShortURL(req,res){
+    const body=req.body
+    if(!body.url) return res.status(401).json({error:'url is required'})
+    const shortId=shortid();
+    await URL.create({
+        shortId:shortId,
+        redirectURL:body.url,
+        visitHistory:[]  
+    })
+
+    return res.json({id:shortId})
+
+}
+
+
+async function handleGetAnalytics(req,res){
+    const shortId=req.params.shortId
+    const result=await URL.findOne({shortId})
+    return res.json({
+        totalClicks:result.visitHistory.length,
+        analytics:result.visitHistory
+    })
+
+}
+
+
+
+module.exports={handleGenerateNewShortURL,handleGetAnalytics}
