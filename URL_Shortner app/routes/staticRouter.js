f7545e310899aa2e2cd302e5e12554d9f9e46852
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-19 10:43:57 +0530

    Adding staticRouter file to render the page

---
+++ URL_Shortner app/routes/staticRouter.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const router=express.Router()
+const URL=require('../models/url')
+
+router.get('/',async(req,res)=>{
+    const allurls=await URL.find({})
+    return res.render('home',{
+        urls:allurls
+    })
+})
+
+
+module.exports=router
