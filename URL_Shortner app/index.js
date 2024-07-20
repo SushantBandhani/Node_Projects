747120c0071376dfd86de34b0ef99018d4a87e3d
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-20 12:10:37 +0530

    Added routes for views

--- URL_Shortner app/index.js
+++ URL_Shortner app/index.js
@@ -1,7 +1,9 @@
 const express=require("express")
 const urlRoute=require('./routes/url')
+const path=require("path")
 const {connectTomongoDb}=require('./connect')
 const URL=require('./models/url')
+const staticRoute=require('./routes/staticRouter')
 const app=express()
 const PORT=8001
 
@@ -9,9 +11,24 @@ connectTomongoDb('mongodb://localhost:27017/short-url').then(()=>{
     console.log("Mongo Db connected")
 })
 
+
+app.get('/test',async(req,res)=>{
+   const allUrls=await URL.find({})
+   return res.render('home',{  
+    urls:allUrls
+   })
+   })
+
+
 app.use(express.json()); // To parse JSON bodies
+app.use(express.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+app.set("views",path.resolve("./views"))
+
 
 app.use('/url',urlRoute);
+app.use('/',staticRoute)
 app.get('/:shortId',async(req,res)=>{
     const shortId=req.params.shortId
    const entry= await URL.findOneAndUpdate({
@@ -28,5 +45,5 @@ app.get('/:shortId',async(req,res)=>{
 
 
 app.listen(8001,()=>{
-    console.log(`Server started at PORT:${PORT}`)
+    console.log(`Server started at PORT :${PORT}`)
 })
