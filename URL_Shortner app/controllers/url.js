4314a496118b6b7fc901e858f272529f60069b04
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 11:24:14 +0530

    updated functions regarding generating url and handling login and signup

--- URL_Shortner app/controllers/url.js
+++ URL_Shortner app/controllers/url.js
@@ -1,33 +1,30 @@
 const shortid=require("shortid")
-
-//Importing database
 const URL=require("../models/url")
 
-async function handleGenerateNewShortURL(req,res){
-    const body=req.body
-    if(!body.url) return res.status(401).json({error:'url is required'})
-    const shortId=shortid();
+async function handleGenerateNewShortURL(req, res) {
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: "url is required" });
+    const shortID = shortid();
+  
     await URL.create({
-        shortId:shortId,
-        redirectURL:body.url,
-        visitHistory:[]  
-    })
-    return res.render('home',{
-        id:shortId
-    })
-}
+      shortId: shortID,
+      redirectURL: body.url,
+      visitHistory: [],
+      createdBy: req.user._id,
+    });
+  
+    return res.render("home", {
+      id: shortID,
+    });
+  }
 
-
-async function handleGetAnalytics(req,res){
-    const shortId=req.params.shortId
-    const result=await URL.findOne({shortId})
+  async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
     return res.json({
-        totalClicks:result.visitHistory.length,
-        analytics:result.visitHistory
-    })
-
-}
-
-
+      totalClicks: result.visitHistory.length,
+      analytics: result.visitHistory,
+    });
+  }
 
 module.exports={handleGenerateNewShortURL,handleGetAnalytics}
