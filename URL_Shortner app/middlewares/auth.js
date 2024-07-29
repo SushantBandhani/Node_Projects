f8fbcb8ebaba8f2824d40726b230bafd7d3f9bdc
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 11:27:34 +0530

    Formatted auth file

--- URL_Shortner app/middlewares/auth.js
+++ URL_Shortner app/middlewares/auth.js
@@ -1,33 +1,26 @@
-const {getUser}=require('../service/auth')
+const { getUser } = require("../service/auth");
 
-async function restrictToLoggedUserOnly(req,res,next){
-    const userUid=req.cookies?.uid 
-    console.log(req)
+async function restrictToLoggedUserOnly(req, res, next) {
+  const userUid = req.cookies?.uid;
 
-    if(!userUid){
-        console.log("yes")   
-        return res.redirect("/login") 
-    }
-    const user=getUser(userUid)
-    if(!user) {
-        console.log("ok")
-        return res.redirect('/login')
-    }
-    req.user=user
-    next();
+  if (!userUid) return res.redirect("/login");
+  const user = getUser(userUid);
 
+  if (!user) return res.redirect("/login");
+
+  req.user = user;
+  next();
 }
 
-async function checkAuth(req,res,next){
-    const userUid=req.cookies?.uid 
-    // console.log(req)
-    const user=getUser(userUid)
-    req.user=user
-    next();
-
+async function checkAuth(req, res, next) {
+  const userUid = req.cookies?.uid;
+  // console.log(req)
+  const user = getUser(userUid);
+  req.user = user;
+  next();
 }
 
-module.exports={
-    restrictToLoggedUserOnly,
-    checkAuth
-}
+module.exports = {
+  restrictToLoggedUserOnly,
+  checkAuth,
+};
