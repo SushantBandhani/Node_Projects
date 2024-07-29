01ee12626b52a5cb7900f0a14b74b92467b6d79a
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 08:30:34 +0530

    mainuser.js

---
+++ URL_Shortner app/routes/user.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const router=express.Router()
+const {handleUserSignup,handleUserLogin}=require('../controllers/user')
+
+router.post('/',handleUserSignup)
+router.post('/login',handleUserLogin)
+module.exports=router
