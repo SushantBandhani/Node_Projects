97a715433fbff3b5627d66f56678429b08d11f1f
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 08:43:51 +0530

    Adding service to check for authenticated uid

---
+++ URL_Shortner app/service/auth.js
@@ -0,0 +1,13 @@
+const sessionIdtoUserMap=new Map()
+
+function setUser(id,user){
+    sessionIdtoUserMap.set(id,user)
+}
+function getUser(id){
+    return sessionIdtoUserMap.get(id)
+}
+
+module.exports={
+    setUser,
+    getUser
+} 
