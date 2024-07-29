ec88ee97f9a54766fb74713f072cae69955b2c03
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 11:28:18 +0530

    Updated staticRouter.js

--- URL_Shortner app/routes/staticRouter.js
+++ URL_Shortner app/routes/staticRouter.js
@@ -3,10 +3,24 @@ const router=express.Router()
 const URL=require('../models/url')
 
 router.get('/',async(req,res)=>{
-    const allurls=await URL.find({})
+    if(!req.user) {
+        return res.redirect('/login')
+    }
+    const allurls=await URL.find({createdBy:req.user._id})
     return res.render('home',{
         urls:allurls
     })
+})
+
+
+// just to render signup page
+router.get('/signup',(req,res)=>{
+    return res.render('signup')
+})
+
+//Just to render login page
+router.get('/login',(req,res)=>{
+    return res.render('login')
 })
 
 
