a9917bfcf98e19112915d671c18eb11a8a2e7bd3
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-07-29 11:31:26 +0530

    Updated index file

--- URL_Shortner app/index.js
+++ URL_Shortner app/index.js
@@ -1,49 +1,47 @@
-const express=require("express")
-const urlRoute=require('./routes/url')
-const path=require("path")
-const {connectTomongoDb}=require('./connect')
-const URL=require('./models/url')
-const staticRoute=require('./routes/staticRouter')
-const app=express()
-const PORT=8001
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
 
-connectTomongoDb('mongodb://localhost:27017/short-url').then(()=>{
-    console.log("Mongo Db connected")
-})
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
+const app = express();
+const PORT = 8001;
 
-app.get('/test',async(req,res)=>{
-   const allUrls=await URL.find({})
-   return res.render('home',{  
-    urls:allUrls
-   })
-   })
+connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.use(express.json()); // To parse JSON bodies
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.set("view engine","ejs")
-app.set("views",path.resolve("./views"))
+app.use("/url", restrictToLoggedUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
-
-app.use('/url',urlRoute);
-app.use('/',staticRoute)
-app.get('/:shortId',async(req,res)=>{
-    const shortId=req.params.shortId
-   const entry= await URL.findOneAndUpdate({
-        shortId
-    },{
-        $push:{
-         visitHistory:{
-            timestamp:Date.now()
-        },   
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
         },
-    })
-    res.redirect(entry.redirectURL)
-})
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
-
-app.listen(8001,()=>{
-    console.log(`Server started at PORT :${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
