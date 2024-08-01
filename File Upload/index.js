aafa8e419b3a23935992a36561e01bec0cfc4659
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-08-01 08:06:29 +0530

    Adding uploading feature

--- File Upload/index.js
+++ File Upload/index.js
@@ -1,13 +1,34 @@
 const express=require('express')
 const path=require("path")
+const multer=require("multer")
+// const upload = multer({ dest: 'uploads/' })
 
 const app=express()
 const PORT=8000
+
+const storage=multer.diskStorage({
+    destination:function(req,res,cb){
+        return cb(null,'./uploads')
+    },
+    filename:function(req,file,cb){
+        return cb(null,`${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload=multer({storage:storage})
 
 app.set("view engine","ejs")
 app.set("views",path.resolve("./views"))
 
 app.use(express.json())
+app.use(express.urlencoded({extended:false})) // used to parse form data
+
+app.post("/upload",upload.single("profileImage"),(req,res)=>{
+    console.log(req.body)
+    console.log(req.file)
+
+    return res.redirect("/")
+})
 
 app.get("/",(req,res)=>{
     return res.render("homepage")
