dc1f03dc55ae22aa64f5c4c55bbfd1269b07953c
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-08-02 10:39:31 +0530

    Updating index.js

--- Discord_bot/index.js
+++ Discord_bot/index.js
@@ -7,10 +7,22 @@ client.on('messageCreate', (message) => {
     // console.log(message)
     // console.log(message.content);
     if(message.author.bot)return;
+    if(message.content.startsWith('create')){
+        const url = message.content.split('create')[1]
+        return message.reply({
+            content:'Generating short id for ' +url,
+        })
+    }
+
     message.reply({
         content:"hi from bot"
     })
 });
+
+client.on('interactionCreate',(interaction)=>{
+    // console.log(interaction)
+    interaction.reply("Pong!!")
+})
 
 const TOKEN = process.env.TOKEN;
 client.login(TOKEN);
