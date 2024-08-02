4faa6537c64142ec2284ce5546d55e6cba025a92
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-08-02 09:45:20 +0530

    Initializing bot

--- Discord_bot/index.js
+++ Discord_bot/index.js
@@ -1,7 +1,16 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages] });
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
 
 client.on('messageCreate', (message) => {
-    console.log(`Logged in as ${message.content}!`);
-  });
+    // console.log(message)
+    // console.log(message.content);
+    if(message.author.bot)return;
+    message.reply({
+        content:"hi from bot"
+    })
+});
+
+const TOKEN = process.env.TOKEN;
+client.login(TOKEN);
