b072789871fa31df7f5e1c98e27bfe59bf63a3a1
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-04 11:13:27 +0530

    Updating routes

--- URL_Shortner app/routes/staticRouter.js
+++ URL_Shortner app/routes/staticRouter.js
@@ -1,11 +1,10 @@
 const express=require('express')
 const router=express.Router()
 const URL=require('../models/url')
+const { chechForAuthentication, restrictTo } = require("../middlewares/auth");
 
-router.get('/',async(req,res)=>{
-    if(!req.user) {
-        return res.redirect('/login')
-    }
+
+router.get('/',restrictTo(["NORMAL"]),async(req,res)=>{
     const allurls=await URL.find({createdBy:req.user._id})
     return res.render('home',{
         urls:allurls
