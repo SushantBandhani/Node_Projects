811711694df8a2c720759ff55158bac2eaad940c
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-04 11:16:17 +0530

    Updating service auth

--- URL_Shortner app/service/auth.js
+++ URL_Shortner app/service/auth.js
@@ -4,7 +4,8 @@ const secret="SUShant$123@54$%"
 function setUser(user){
     const payload={
         _id:user._id,
-        email:user.email
+        email:user.email,
+        role:user.role
     }
 
    return jwt.sign(payload,secret)
