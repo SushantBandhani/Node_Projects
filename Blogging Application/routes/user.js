53d5876f1abee6bd8faf906b6144409980a7c32d
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-07 21:16:28 +0530

    Adding signin POST Route

--- Blogging Application/routes/user.js
+++ Blogging Application/routes/user.js
@@ -5,6 +5,18 @@ const User=require("../models/user")
 router.get('/signin',(req,res)=>{
     return res.render("signin")
 })
+
+router.post('/signin',async(req,res)=>{
+    const{email,password}=req.body
+  const user= User.matchpassword(email,password)
+  if(!user){
+    res.send({error:"Incorrect username or password"})
+  }
+  console.log("User",user)
+    return res.redirect("/")  //redirecting to homepage
+})
+
+
 router.get('/signup',(req,res)=>{
     return res.render("signup")
 })
