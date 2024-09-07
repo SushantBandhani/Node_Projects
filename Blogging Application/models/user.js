7f23adcd5d5ca786e3db1cf81ae3de1026a2a20c
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-07 14:38:04 +0530

    Updating schema

--- Blogging Application/models/user.js
+++ Blogging Application/models/user.js
@@ -1,46 +1,51 @@
 const {Schema,model}=require("mongoose")
 const {createHmac,randomBytes}=require("node:crypto")
-const userSchema= new Schema({
-    firstName:{
-        type:String,
-        required:true
+const userSchema = new Schema(
+    {
+      fullName: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      salt: {
+        type: String,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      profileImageURL: {
+        type: String,
+        default: "/images/default.png",
+      },
+      role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+      },
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    salt:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    profileImageURL:{
-        type:String,
-        default:'/images/default.png'
-    },
-    role:{
-        type:String,
-        enum:["USER","ADMIN"],
-        default:"USER"
-    }
-},{timestamps:true})
-
-userSchema.pre("save",function(next){
-    const user=this
-    if(!user.isModified("password")) return
-
-    //salt generation
-    const salt=randomBytes(16).toString()  // secret key
-    const hashedPassword=createHmac('sha256',salt ).update(user.password).digest("hex")
-
-    this.salt=salt
-    this.password=hashedPassword
-
-})
+    { timestamps: true }
+  );
+  
+  userSchema.pre("save", function (next) {
+    const user = this;
+  
+    if (!user.isModified("password")) return;
+  
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+      .update(user.password)
+      .digest("hex");
+  
+    this.salt = salt;
+    this.password = hashedPassword;
+  
+    next();
+  });
 
 const User=model('user',userSchema)
 module.exports=User
