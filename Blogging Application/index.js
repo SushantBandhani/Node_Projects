479486eaff82b6e87e1fa629baa929f07b54ef19
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-09 11:17:14 +0530

    Adding cookie authentication middleware and also added /blog route and updated package files

--- Blogging Application/index.js
+++ Blogging Application/index.js
@@ -4,7 +4,11 @@ const app=express()
 const PORT=8000
 const mongoose=require('mongoose')
 const userRoute=require("./routes/user")
+const blogRoute=require("./routes/blog")
 const bodyParser = require('body-parser');
+const cookieParser=require('cookie-parser')
+const Blog=require('./models/blog')
+const {checkForAuthenticationCookie}=require("./middlewares/authentication")
 mongoose.connect('mongodb://localhost:27017/blogify').then((e)=>{
     console.log('Mongodb connected')
 })
@@ -16,12 +20,20 @@ app.set('views',path.resolve('./views'))
 
 //to need to handle form data
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());    //To parse cookie
+app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve("./public")))
 
-app.get("/",(req,res)=>{
-    res.render('home')
+app.get("/",async (req,res)=>{
+    const allBlogs=await Blog.find({})
+    res.render('home',{
+        user:req.user,
+        blogs:allBlogs
+    })
 })
 
 app.use("/user",userRoute)
+app.use("/blog",blogRoute)
 app.listen(PORT,()=>{
     console.log(`server started at PORT: ${PORT}`)
 })
