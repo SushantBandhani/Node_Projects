9d4e5162f944c4819b942d9bc8f6f04cdeda1cc2
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-14 13:35:23 +0530

    Updating index.js

--- Chat App/index.js
+++ Chat App/index.js
@@ -1,10 +1,22 @@
 const express=require('express')
 const http=require('http')
 const app=express()
-
+const {Server} =require("socket.io")
+const path=require('path')
 const server=http.createServer(app)
 
+app.use(express.static(path.resolve('./public')))
 
+const io=new Server(server)
+io.on('connection',(socket)=>{
+    socket.on('user-message',(message)=>{
+        io.emit("message",message)
+    })
+})
+
+app.get('/',(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
 
 server.listen(9000,()=>{
     console.log(`server started at PORT 9000`)
